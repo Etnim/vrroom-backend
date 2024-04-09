9acc413b5ce92baa44ff0e566e5b16da914e524d
LinaAnule <152215917+LinaAnule@users.noreply.github.com>
2024-04-09 16:57:14 +0300

    added swagger

--- src/main/java/com/vrrom/VRromApplication.java
+++ src/main/java/com/vrrom/VRromApplication.java
@@ -2,8 +2,9 @@ package com.vrrom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class VRromApplication {
 
 	public static void main(String[] args) {
