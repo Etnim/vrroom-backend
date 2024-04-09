3ba2225a726ae29a880387ee28a5bd192f88a6cc
Anhelina Velychko <avelichko469@gmail.com>
2024-04-09 15:33:48 +0300

    add empty SpringBoot app

---
+++ src/main/java/com/vrrom/VRromApplication.java
@@ -0,0 +1,13 @@
+package com.vrrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VRromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VRromApplication.class, args);
+	}
+
+}
