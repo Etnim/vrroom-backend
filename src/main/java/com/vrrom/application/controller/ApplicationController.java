db5b938199b146ac86c3b21d6945322685b6f6dd
Anhelina Velychko <avelichko469@gmail.com>
2024-04-15 20:44:54 +0300

    Added dto objects and validations

--- src/main/java/com/vrrom/application/controller/ApplicationController.java
+++ src/main/java/com/vrrom/application/controller/ApplicationController.java
@@ -1,8 +1,13 @@
 package com.vrrom.application.controller;
 
+import com.vrrom.application.model.Application;
+import com.vrrom.application.model.ApplicationDTO;
 import com.vrrom.application.service.ApplicationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,10 @@ public class ApplicationController {
     @Autowired
     public ApplicationController(ApplicationService applicationService) {
         this.applicationService = applicationService;
+    }
+    @PostMapping
+    public ResponseEntity<Application> createApplication(@RequestBody ApplicationDTO applicationDTO) {
+        Application createdApplication = applicationService.createApplication(applicationDTO);
+        return ResponseEntity.ok(createdApplication);
     }
 }
