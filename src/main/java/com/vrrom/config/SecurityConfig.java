1e71539c09f631be2b0283487943681155df5d99
ufuksaylan <ufuksaylannn@gmail.com>
2024-04-30 19:39:06 +0300

    disable csrf for public post request

--- src/main/java/com/vrrom/config/SecurityConfig.java
+++ src/main/java/com/vrrom/config/SecurityConfig.java
@@ -1,28 +1,30 @@
-package com.vrrom.config;
+    package com.vrrom.config;
 
-import com.vrrom.security.FirebaseAuthenticationTokenFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+    import com.vrrom.security.FirebaseAuthenticationTokenFilter;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.http.HttpMethod;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+    @Configuration
+    @EnableWebSecurity
+    public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/cars/{make}/models").permitAll()
-                        .requestMatchers("/cars/makes").permitAll()
-                        .requestMatchers("/euribor/{term}").permitAll()
-                        .requestMatchers("/applications/application").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(new FirebaseAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authz -> authz
+                            .requestMatchers("/cars/{make}/models").permitAll()
+                            .requestMatchers("/cars/makes").permitAll()
+                            .requestMatchers("/euribor/{term}").permitAll()
+                            .requestMatchers("/applications/application").permitAll()
+                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                            .anyRequest().authenticated()
+                    )
+                    .addFilterBefore(new FirebaseAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+            return http.build();
+        }
     }
-}
