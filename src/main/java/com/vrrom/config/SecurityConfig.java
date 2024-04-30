3694193d87f3daf0e23effebb0a4be8731db52ef
ufuksaylan <66475538+ufuksaylan@users.noreply.github.com>
2024-04-30 20:04:45 +0300

    disable csrf for public post request (#43)

--- src/main/java/com/vrrom/config/SecurityConfig.java
+++ src/main/java/com/vrrom/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -14,7 +15,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz -> authz
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authz -> authz
                         .requestMatchers("/cars/{make}/models").permitAll()
                         .requestMatchers("/cars/makes").permitAll()
                         .requestMatchers("/euribor/{term}").permitAll()
